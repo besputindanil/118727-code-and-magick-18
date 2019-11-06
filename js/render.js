5aac7ae1afc675c0ded0b6eeafa1b7b715db6196
besputindanil <50333644+besputindanil@users.noreply.github.com>
2019-11-06 19:43:42 +0400

    Реализована загрузка аватара

--- js/render.js
+++ js/render.js
@@ -9,8 +9,8 @@
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorEyes;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill  = wizard.colorEyes;
 
     return wizardElement;
   };
